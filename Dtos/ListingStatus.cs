f74c7a12e62b8f782a0fbc36a71c1fd4f3f18df6
kifle23 <kiflekifle23@gmail.com>
2024-05-03 18:24:13 +0300

    Create Dto for Listing status

---
+++ Dtos/ListingStatus.cs
@@ -0,0 +1,11 @@
+namespace AsyncProductAPI.Dtos
+{
+    public class ListingStatus
+    {
+        public string? RequestStstus { get; set; }
+        public string? EstimatedCompletionTime { get; set; }
+
+        public string? ResourceURL { get; set; }
+
+    }
+}
